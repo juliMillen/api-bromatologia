ffc9d0c7f16e8728e92f7769c09804e218183da9
Julian Millen <julimillen@gmail.com>
2025-08-20 11:46:38 -0300

    correccion commit anterior: actualizando cors para que acepte url de netlify.

--- backend/src/main/java/com/bromatologia/backend/Configuration/CorsConfig.java
+++ backend/src/main/java/com/bromatologia/backend/Configuration/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // a todas las rutas
-                .allowedOrigins("https://api-bromatologia.onrender.com") // direccion al path de render
+                .allowedOrigins("https://bromatologia.netlify.app") // path netlify
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE","OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
